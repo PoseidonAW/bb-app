e47eae387c8cc055d1a3386e36f8c80c0382e6bf
Jim Dev <jimdev@SixSquared.home>
2016-07-09 16:30:42 -0500

    Account model and related files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blissbusiness::Application.routes.draw do
+  resources :accounts
+
   get "dashboard/index"
   root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
